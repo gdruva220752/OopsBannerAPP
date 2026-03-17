7fb15c7455f85e439758dfa1d85f5d344c3916c5
gdruva220752 <target220.inmains@gmail.com>
2026-03-18 00:58:06 +0530

    Update Printapp.java

--- Printapp.java
+++ Printapp.java
@@ -1,13 +1,33 @@
-public class UC2_OOPS {
+/**
+ * OOPSBannerApp - UC3 Implementation
+ * Prints "OOPS" banner using String.join()
+ * 
+ * @author Nani
+ * @version 3.0
+ */
+public class OOPSBannerApp {
+
     public static void main(String[] args) {
 
-        System.out.println(" *****    *****    *****    ***** ");
-        System.out.println("*     *  *     *  *     *  *     *");
-        System.out.println("*     *  *     *  *     *  *      ");
-        System.out.println("*     *  *     *   *****    ***** ");
-        System.out.println("*     *  *     *        *        *");
-        System.out.println("*     *  *     *  *     *  *     *");
-        System.out.println(" *****    *****    *****    ***** ");
+        System.out.println(String.join("   ",
+                " ***** ", " ***** ", " ***** ", " ***** "));
 
+        System.out.println(String.join("   ",
+                "*     *", "*     *", "*     *", "*     *"));
+
+        System.out.println(String.join("   ",
+                "*     *", "*     *", "*     *", "*     *"));
+
+        System.out.println(String.join("   ",
+                "*     *", " ***** ", " ***** ", " ***** "));
+
+        System.out.println(String.join("   ",
+                "*     *", "*      ", "*            ", "     *"));
+
+        System.out.println(String.join("   ",
+                "*     *", "*      ", "*            ", "     *"));
+
+        System.out.println(String.join("   ",
+                " ***** ", "*      ", " ***** ", " ***** "));
     }
 }
